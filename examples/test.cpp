3a1ce18625742b4945157a9983c3ce135a4dc2e5
ZhuZiyi <zyzhu2001@gmail.com>
2023-07-13 07:44:39 +0000

    style: change code format
    Signed-off-by: ZhuZiyi <zyzhu2001@gmail.com>

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
+#include <greptime/v1/column.pb.h>
 #include <src/database.h>
 void test_hello() {
     greptime::Database database;
     database.hello();
 }
 int main () {
-
     test_hello();
-
     return 0;
 }
